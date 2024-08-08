b4d0223504a2bfc8664c595fdc371213966b9e84
Sophia <sophia.tsau@gmail.com>
2024-08-08 11:33:10 -0700

    generation_config for the last 2 feedback bots

--- app/utils/ai_utils.py
+++ app/utils/ai_utils.py
@@ -45,17 +45,17 @@ dictionary_bot = genai.GenerativeModel(
 # social context bot
 social_context_bot = genai.GenerativeModel(
     model_name="gemini-1.5-pro",
-    # generation_config=generate_config({
-    #     "response": "str"
-    # }),
+    generation_config=generate_config(TypedDict("SocialContextSchema", {
+        "response": "str"
+    })),
     system_instruction="Given a message, message history, and social context in the format of {message:message, messageHistory: [{role:role, text:message}], socialContext: {user: messageSender, receiver: messageReceiver, situation: situation}}, generate a response that analyzes how the user's message might be received in the context of the message history and how appropriate the message is. The response should include information about the social norms and expectations that are relevant to the situation presented in the message history and social context.",
 )
 
 # example response bot
 example_response_bot = genai.GenerativeModel(
     model_name="gemini-1.5-pro",
-    # generation_config=generation_config,
-    system_instruction="Given a message, message history, and social context in the format of {message:message from conversation partner, messageHistory: [{role:role, text:message}], socialContext: {user: messageReceiver, conversationPartner: messageSender, situation: situation}},  generate an example response to the message that the user could use in the given social context. The response should include an explanation regarding what the conversation partner might expect from the user's response and what the model's example response communicates. The response should be formatted as {response: response, explanation: explanation}.",
+    generation_config=generate_config(TypedDict("ExampleResponseSchema", {"example_response": "str", "explanation": "str"})),
+    system_instruction="Given a message, message history, and social context in the format of {message:message from conversation partner, messageHistory: [{role:role, text:message}], socialContext: {user: messageReceiver, conversationPartner: messageSender, situation: situation}},  generate an example response to the message that the user could use in the given social context. The response should include an explanation regarding what the conversation partner might expect from the user's response and what the model's example response communicates.",
 )
 
 
