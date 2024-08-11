822c32cf73be6f506dd2bb6aea30d2ec789f5d88
RobertoPeregrinaJr <robertoperegrina1996@gmail.com>
2024-08-11 15:32:11 -0700

    starting auth_route

--- tests/backend/auth_route.spec.js
+++ tests/backend/auth_route.spec.js
@@ -1,1 +1,16 @@
- 
+const { test, expect } = require("@playwright/test");
+
+test.describe("Flask App", () => {
+  // endpoint auth is located
+  const baseURL = "http://127.0.0.1:8000";
+
+  test("should Authenticates a user.", async ({ request }) => {
+    const response = await request.get(`${baseURL}/api/`, {});
+    console.log("=========".repeat(4));
+    console.log("response: ", response);
+    console.log("=========".repeat(4));
+
+    expect(response.status()).toBe(200);
+    expect(response)
+  });
+});
